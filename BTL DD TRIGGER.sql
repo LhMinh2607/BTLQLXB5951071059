ca2084e6a54e431303ea53ecd390a2b6fc7b9469
LhMinh2607 <68017090+LhMinh2607@users.noreply.github.com>
2020-12-15 10:21:44 +0700

    Update BTL DD TRIGGER.sql

--- BTL DD TRIGGER.sql
+++ BTL DD TRIGGER.sql
@@ -1,9 +1,9 @@
-﻿USE BusManager
+USE BusManager
 GO
 
 --TRIGGER
 
---1. Kiểm tra input của Staff (Nhân viên)
+--1.1. Kiểm tra input của Staff (Nhân viên)
 CREATE TRIGGER tr_StaffInput
 ON STAFF
 	FOR INSERT
@@ -19,9 +19,9 @@ ON STAFF
 						PRINT N'Chức vụ không hợp lệ.'
 						ROLLBACK TRAN
 						END
-				IF(((SELECT Chucvu FROM inserted)=N'Tiếp Viên') AND ((SELECT So_Ve_Ban_Ra_Trong_Ngay FROM inserted) < 50))
+				IF((SELECT So_Ve_Ban_Ra_Trong_Ngay FROM inserted) < 0)
 					BEGIN
-						PRINT N'Số vé phải lớn hơn hoặc bằng 50 đối với tiếp viên.'
+						PRINT N'Số vé phải lớn hơn hoặc bằng 0.'
 						ROLLBACK TRAN
 						END
 				IF((SELECT Luong FROM inserted)<6900000)
@@ -38,13 +38,13 @@ ON STAFF
 	
 DROP TRIGGER tr_StaffInput
 --Test Trigger Input của Nhân viên
-INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Cis',N'Tiếp Viên',80)
-INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Lơ xe',80)
-INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',40)
-INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',300000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',80)
-INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/2005',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',80)
+INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Cis',N'Tiếp Viên',0)
+INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Lơ xe',0)
+INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',-10)
+INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/1970',300000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',0)
+INSERT INTO STAFF VALUES ('08A420','08',N'Nguyễn Nguyên Nguyện','9/6/2005',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',0)
 
---2. Kiểm tra sửa của Staff (Nhân viên)
+--1.2. Kiểm tra sửa của Staff (Nhân viên)
 CREATE TRIGGER tr_StaffUpdate
 ON STAFF
 	FOR UPDATE
@@ -60,9 +60,9 @@ ON STAFF
 						PRINT N'Chức vụ không hợp lệ.'
 						ROLLBACK TRAN
 						END
-				IF(((SELECT Chucvu FROM inserted)=N'Tiếp Viên') AND ((SELECT So_Ve_Ban_Ra_Trong_Ngay FROM inserted) < 50))
+				IF((SELECT So_Ve_Ban_Ra_Trong_Ngay FROM inserted) < 0)
 					BEGIN
-						PRINT N'Số vé phải lớn hơn hoặc bằng 50 đối với tiếp viên.'
+						PRINT N'Số vé phải lớn hơn hoặc bằng 0.'
 						ROLLBACK TRAN
 						END
 				IF((SELECT Luong FROM inserted)<6900000)
@@ -78,15 +78,15 @@ ON STAFF
 			END
 DROP TRIGGER tr_StaffUpdate
 --Test Trigger tr_StaffUpdate
-INSERT INTO STAFF VALUES ('08A421','08',N'Nguyễn Thử Nghiệm','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',50)
-UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Cis', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=80 WHERE MaNV='08A421'
-UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Lơ xe',So_ve_ban_ra_trong_ngay=80  WHERE MaNV='08A421'
-UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=40  WHERE MaNV='08A421'
-UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=110000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=80  WHERE MaNV='08A421'
-UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/2005', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=80 WHERE MaNV='08A421'
+INSERT INTO STAFF VALUES ('08A421','08',N'Nguyễn Thử Nghiệm','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',0)
+UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Cis', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=0 WHERE MaNV='08A421'
+UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Lơ xe',So_ve_ban_ra_trong_ngay=0  WHERE MaNV='08A421'
+UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=-10  WHERE MaNV='08A421'
+UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/1970', Luong=110000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=0  WHERE MaNV='08A421'
+UPDATE STAFF SET MaTuyen='08',HoTen=N'Nguyễn Nguyên Nguyện', NgaySinh='9/6/2005', Luong=11000000, DiaChi=N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM', GioiTinh= N'Nam', Chucvu=N'Tiếp Viên',So_ve_ban_ra_trong_ngay=0 WHERE MaNV='08A421'
 
 
---3 Trigger xóa staff
+--1.3 Trigger xóa staff
 CREATE TRIGGER tr_StaffRemove
 ON STAFF
 	INSTEAD OF DELETE
@@ -96,11 +96,11 @@ ON STAFF
 				DELETE STAFF WHERE MaNV=(SELECT MaNV FROM deleted)
 			END
 DROP TRIGGER tr_StaffRemove
-INSERT INTO STAFF VALUES ('69','08',N'Nguyễn Ngôn','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',80)
+INSERT INTO STAFF VALUES ('69','08',N'Nguyễn Ngôn','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',0)
 DELETE STAFF WHERE MaNV='69'
 
 
---1. Kiểm tra nhập của BUS (Xe buýt)
+--2.1. Kiểm tra nhập của BUS (Xe buýt)
 CREATE TRIGGER tr_BusInput
 ON BUS
 	FOR INSERT
@@ -136,7 +136,7 @@ INSERT INTO BUS VALUES ('420','0301410987',N'Bến xe buýt Weeds',90,19,348,'4:
 INSERT INTO BUS VALUES ('420','0301410987',N'Bến xe buýt Weeds',90,80,19,'4:40','20:30')
 
 
---2. Kiểm tra sửa của BUS (Xe buýt)
+--2.2. Kiểm tra sửa của BUS (Xe buýt)
 CREATE TRIGGER tr_BusUpdate
 ON BUS
 	FOR UPDATE
@@ -175,7 +175,7 @@ UPDATE BUS SET MaHTX='0301410987', TenTuyen=N'Bến xe buýt Grass+1',ThoiGian_M
 --UPDATE BUS SET MaHTX='0301410987', TenTuyen=N'Bến xe buýt Grass+1',ThoiGian_MotChuyen=90,SoChoNgoi=80,SoChuyen=348,KhoiHanh='2:30',KetThuc='14:40' WHERE MaTuyen='421'
 
 
---3. Trigger xóa của BUS (Xe buýt)
+--2.3. Trigger xóa của BUS (Xe buýt)
 CREATE TRIGGER tr_BusRemove
 ON BUS
 	INSTEAD OF DELETE
@@ -192,13 +192,13 @@ DROP TRIGGER tr_BusRemove
 
 --Test Trigger Xóa Xe buýt
 INSERT INTO STAFF VALUES ('421A69','421',N'Trần Thử Nghiệm','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',50)
-INSERT INTO TICKET_PACK VALUES ('421HS01','421A69',3000,N'Học Sinh Sinh Viên')
-INSERT INTO TICKET_PACK VALUES ('421HS02','421A69',3000,N'Vé Thường')
+INSERT INTO TICKET_PACK VALUES ('421HS01','421A69',3000,N'Học Sinh Sinh Viên', 50)
+INSERT INTO TICKET_PACK VALUES ('421HS02','421A69',6000,N'Vé Thường', 50)
 INSERT INTO CROSS_STATION VALUES (N'421','BX87',3,0)
 INSERT INTO CROSS_STATION VALUES (N'421','QTD 177',3,1)
 DELETE BUS WHERE MaTuyen='421'
 
---1. Trigger xóa HTX
+--3.1. Trigger xóa HTX
 CREATE TRIGGER tr_HTXRemove
 ON HTX
 	INSTEAD OF DELETE
@@ -215,10 +215,54 @@ DROP TRIGGER tr_HTXRemove
 INSERT INTO HTX VALUES ('420420420',N'Hợp Tác Xã Vận Tải Xe Buýt Weeds',N'1129 Lạc Long Quân P.11 Q.Tân Bình TP.HCM')
 INSERT INTO BUS VALUES ('42','420420420',N'Bến xe buýt Weeds+1',90,80,29,'4:40','20:30')
 INSERT INTO STAFF VALUES ('42A69','42',N'Trần Thử Nghiệm','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',50)
-INSERT INTO TICKET_PACK VALUES ('42HS01','42A69',3000,N'Học Sinh Sinh Viên')
-INSERT INTO TICKET_PACK VALUES ('42HS02','42A69',3000,N'Vé Thường')
+INSERT INTO TICKET_PACK VALUES ('42HS01','42A69',3000,N'Học Sinh Sinh Viên', 50)
+INSERT INTO TICKET_PACK VALUES ('42HS02','42A69',3000,N'Vé Thường', 50)
 INSERT INTO CROSS_STATION VALUES (N'42','BX87',3,0)
 INSERT INTO CROSS_STATION VALUES (N'42','QTD 177',3,1)
 DELETE HTX WHERE MaHTX='420420420'
 
+
+--4.1. Kiểm tra nhập của TICKET_PACK (Vé Xe buýt)
+CREATE TRIGGER tr_TicketInput
+ON TICKET_PACK
+	AFTER INSERT
+		AS
+			BEGIN
+				UPDATE STAFF SET So_Ve_Ban_Ra_Trong_Ngay+=INSERTED.SoVeBan
+				FROM INSERTED
+				WHERE INSERTED.MaNV=STAFF.MaNV
+			END
+
+DROP TRIGGER tr_TicketInput
+INSERT INTO HTX VALUES ('420420420',N'Hợp Tác Xã Vận Tải Xe Buýt Weeds',N'1129 Lạc Long Quân P.11 Q.Tân Bình TP.HCM')
+INSERT INTO BUS VALUES ('42','420420420',N'Bến xe buýt Weeds+1',90,80,29,'4:40','20:30')
+INSERT INTO STAFF VALUES ('42A69','42',N'Trần Thử Nghiệm','9/6/1970',11000000,N'10 Lạc Long Quân P.10 Q.Tân Bình, TP.HCM',N'Nam',N'Tiếp Viên',0)
+INSERT INTO TICKET_PACK VALUES ('42HS01','42A69',3000,N'Học Sinh Sinh Viên', 50)
+INSERT INTO TICKET_PACK VALUES ('42PB02','42A69',7000,N'Vé Thường', 50)
+
+--4.2. Kiểm tra XÓA của TICKET_PACK (Vé Xe buýt)
+CREATE TRIGGER tr_TicketRemove
+ON TICKET_PACK
+	AFTER DELETE
+		AS
+			BEGIN
+				UPDATE STAFF SET So_Ve_Ban_Ra_Trong_Ngay-=deleted.SoVeBan
+				FROM DELETED
+				WHERE deleted.MaNV=STAFF.MaNV
+			END
+
+DELETE TICKET_PACK WHERE MaTapVe='42HS01'
+
+--4.3. Kiểm tra SỬA của TICKET_PACK (Vé Xe buýt)
+CREATE TRIGGER tr_TicketUpdate
+ON TICKET_PACK
+	AFTER UPDATE
+		AS
+			BEGIN
+				UPDATE STAFF SET So_Ve_Ban_Ra_Trong_Ngay=So_Ve_Ban_Ra_Trong_Ngay-deleted.SoVeBan+inserted.SoVeBan
+				FROM DELETED, inserted
+				WHERE deleted.MaNV=STAFF.MaNV AND inserted.MaNV=STAFF.MaNV
+			END
+DROP TRIGGER tr_TicketUpdate
+UPDATE TICKET_PACK SET SoVeBan=175 WHERE MaTapVe='42PB02'
 
